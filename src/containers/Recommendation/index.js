fd217e9054a25310e2f28df49139753a80dfb89a
Tore Stensaker Tefre <tore@tefre.it>
2019-09-04 14:09:06 +0200

    added picture

--- src/containers/Recommendation/index.js
+++ src/containers/Recommendation/index.js
@@ -6,6 +6,10 @@ export const Recommendation = () => {
       <h1>You should stay at Lerkendal Student Village</h1>
       <section>
         <h2>Close to campus, night life, and affordable.</h2>
+        <img
+          src="https://www.sit.no/sites/default/files/lerkendal.jpg"
+          alt="Lerkendal studentby"
+        />
       </section>
     </main>
   );
