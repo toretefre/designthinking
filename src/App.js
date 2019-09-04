e2027a7232733427c0aa79338bcb079df810909f
Tore Stensaker Tefre <tore@tefre.it>
2019-09-04 13:30:57 +0200

    added button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './App.css';
 
 export const App = () => {
-  return <h1>Welcome to Easy Student Housing Trondheim</h1>;
+  return (
+    <main>
+      <h1>Welcome to Easy Student Housing Trondheim</h1>
+      <a href="/survey">
+        <button>Find your perfect flat!</button>
+      </a>
+    </main>
+  );
 };
 
 export default App;
